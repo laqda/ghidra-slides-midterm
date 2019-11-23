d2a8a666d28f967e3af4b9cad76361d92c105a57
Quentin Michel <quentinmichel69110@gmail.com>
2019-11-23 21:15:47 +0100

    disable overview

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ ReactDOM.render(<App/>, mountNode);
 
 Reveal.initialize({
   controls: false,
+  overview: false,
   hash: true,
   transitionSpeed: 'fast',
   // margin: -0.3, // to adjust when generating a pdf --- .yarn/bin/decktape http://localhost:1234/#/ Courses/A3/SIS/Ghidra/slides/test.pdf --slides 1-3
