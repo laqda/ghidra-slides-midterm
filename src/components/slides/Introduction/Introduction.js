8b1fe959903adf8910c92510e7a1bca95bd922ed
Quentin Michel <quentinmichel69110@gmail.com>
2019-12-12 11:49:59 +0100

    correct emoji

--- src/components/slides/Introduction/Introduction.js
+++ src/components/slides/Introduction/Introduction.js
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./Introduction.css";
 import {Slide} from "../../Slide/Slide";
 import rsa_conference from "./rsa_conference.png";
-import ReactMarkdown from "react-markdown";
 
 export const introductionTitles = {
   title: "Introduction",
@@ -26,7 +25,18 @@ export const Introduction = () => {
   return (
     <React.Fragment>
       <Slide>
-
+        <div>
+          <h4>étude et analyse d'un système</h4>
+          <div>
+            <ul>
+              <li>comprendre le fonctionnement</li>
+              <li>améliorer la sécurité et la qualité</li>
+              <li>contourner des protections*</li>
+            </ul>
+          </div>
+          <div className={`fragment ${styles.note}`}>* faire de l'open source depuis des codes propriétaires <i className="em em-wink"/>
+          </div>
+        </div>
       </Slide>
       <Slide data-background-color="#361a56">
         <img className={styles.img} src={rsa_conference} alt="rsa-conference"/>
