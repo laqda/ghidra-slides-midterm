6947872c69773fb083ac28cca97dc87ab412c69c
Quentin Michel <quentinmichel69110@gmail.com>
2019-12-12 10:59:18 +0100

    add markdown support

--- src/components/slides/Introduction/Introduction.js
+++ src/components/slides/Introduction/Introduction.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Introduction.css";
 import {Slide} from "../../Slide/Slide";
 import rsa_conference from "./rsa_conference.png";
+import ReactMarkdown from "react-markdown";
 
 export const introductionTitles = {
   title: "Introduction",
@@ -24,6 +25,9 @@ export const introductionTitles = {
 export const Introduction = () => {
   return (
     <React.Fragment>
+      <Slide>
+
+      </Slide>
       <Slide data-background-color="#361a56">
         <img className={styles.img} src={rsa_conference} alt="rsa-conference"/>
       </Slide>
